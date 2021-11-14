a435e09751dedc5ce1ee100814ff4f1aab4beeed
geronimochannelplayer <91771762+geronimochannelplayer@users.noreply.github.com>
2021-11-14 14:12:36 +0530

    Update start.py

--- alita/plugins/start.py
+++ alita/plugins/start.py
@@ -202,7 +202,8 @@ async def help_menu(_, m: Message):
             keyboard = ikb([[("Help", f"t.me/{BOT_USERNAME}?start=help", "url")]])
             msg = tlang(m, "start.pm_for_help")
 
-        await m.reply_photo(photo=f"{random.choice(PHOTO)}",
+        await m.reply_photo(photo=
+                            {random.choice(PHOTO)},
                             caption=msg,
                             reply_markup=keyboard
         )
