1de92f99467ac5b9a36e475ed0444efcfbeb3381
geronimochannelplayer <91771762+geronimochannelplayer@users.noreply.github.com>
2021-11-17 11:01:56 +0530

    Update start_utils.py

--- alita/utils/start_utils.py
+++ alita/utils/start_utils.py
@@ -64,7 +64,7 @@ async def gen_start_kb(q: Message or CallbackQuery):
                     f"https://t.me/{BOT_USERNAME}?startgroup=new",
                     "url",
                 ),
-                (f"🔍 {(tlang(q, 'start.support_group'))}", switch_inline_query_current_chat=''),
+                (f"🔍 {(tlang(q, 'start.support_group'))}", "switch_inline_query_current_chat=' '"),
             ],
             [(f"📚 {(tlang(q, 'start.commands_btn'))}", "commands")],
             [
